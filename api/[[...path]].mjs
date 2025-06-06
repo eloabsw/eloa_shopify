21e53c80fd10991d0593ac2cc75a7f3b3a1ac684
edwin <gerencia@somoscreandola.com>
2025-06-05 23:06:20 -0500

    Limpiar SSR manual, usar output automático de Hydrogen

--- api/[[...path]].mjs
+++
@@ -1,7 +0,0 @@
-export default {
-    async fetch(request, env, context) {
-      const { createRequestHandler } = await import('@shopify/hydrogen/entry-vercel');
-      return createRequestHandler().fetch(request, env, context);
-    },
-  };
-  
