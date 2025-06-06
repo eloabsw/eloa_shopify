b5f2580db66fbbb141a18dc3c445b9ce02f7a6f4
edwin <gerencia@somoscreandola.com>
2025-06-05 22:54:08 -0500

    Fix Hydrogen SSR with official entry-vercel handler

--- api/[[...path]].js
+++ api/[[...path]].js
@@ -1,9 +1,3 @@
 import { createRequestHandler } from '@shopify/hydrogen/entry-vercel';
 
 export default createRequestHandler();
-
-
-// import * as build from '../dist/server/index.js';
-// import { createRequestHandler } from '@remix-run/vercel';
-
-// export default createRequestHandler({ build });
